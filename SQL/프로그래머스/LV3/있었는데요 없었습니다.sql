696e6187f37869ae73d29ef0ce1c96e5c58c962e
rokga <pk123408@naver.com>
2023-03-21 23:53:33 +0900

    3.21 SQL

---
+++ SQL/프로그래머스/LV3/있었는데요 없었습니다.sql
@@ -0,0 +1,15 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/59043
+
+
+
+
+    -- 코드를 입력하세요
+SELECT
+    A.ANIMAL_ID,
+    A.NAME
+FROM ANIMAL_OUTS A
+         JOIN ANIMAL_INS B ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE B.DATETIME > A.DATETIME
+ORDER BY B.DATETIME
+;
+
